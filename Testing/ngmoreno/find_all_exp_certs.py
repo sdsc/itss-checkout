4719c4ba288e96620b9ac5bce7ed48f4083578a0
Nathaniel G Moreno <ngmoreno@shaggy.sdsc.edu>
2015-12-01 10:27:26 -0800

    added exception handling and ability to read DER encoded formats

--- Testing/ngmoreno/find_all_exp_certs.py
+++ Testing/ngmoreno/find_all_exp_certs.py
@@ -12,22 +12,48 @@ ipSpace = dict(
 	test='173.194.207.139' # google.com
 )
 
-com1 = "timeout 5s echo | openssl s_client -connect "
-com2 = ":443 2>/dev/null | openssl x509 -noout -dates"
-# -issuer -subject
+# terminal commands
+com1 = "timeout 2s echo | openssl s_client -connect "
 
-f = open('ssl_checkList', 'w')
+# parses the certificate as if it were DER encoded
+com2 = ":443 2>/dev/null | openssl x509 -inform der -noout -dates"
 
+f = open('ssl_checkList.txt', 'w')
+
+# loops through all IP-Spaces
 for key in ipSpace.keys():
+	
+	# r is a list of all valid IPs in IP-Space
 	r = iptools.IpRangeList(ipSpace[key])
+	
 	for ip in r: 
-		output = subprocess.check_output([com1+ ip +com2], shell=True)
-		nl_index = output.index("\n")
-		result = ip + " " + output[nl_index+1:len(output)-1]
+		# resets value of output
+		output = None
+		
+		# flag for handling error
+		flag = 0
+		
+		# executes terminal command with ip
+		try:
+			output = subprocess.check_output([com1+ ip +com2], shell=True)
+		except subprocess.CalledProcessError as e:
+			output = e.output
+		
+		try:
+			nl_index = output.index("\n")
+		except:
+			result = ip + " - error"
+			flag = 1
+			pass
+		
+		if (not flag):
+			result = ip + " " + output[nl_index+1:len(output)-1]
+		
 		f.write(result)
 f.close()
 
 
+# here for parsing file produced above
 def parseData(in_filename):
         for line in urllib.urlopen(in_filename):
                 yield eval(line)
